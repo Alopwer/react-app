d632144ed1a4f88b69ce43b753d991f0cfe7d5f9
Nikita Pechorin <collidem31@gmail.com>
2019-07-13 10:05:00 +0300

    styling added

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import style from './recipe.module.css'
 
 const Recipe = ({title, calories, image, ingredients}) => {
+
+  let counter = 0;
+
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
       <ol>
         {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
+          <li key={counter++}>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
-      <img src={image} alt={title}/>
+      <p>Number of calories: {Math.round(calories)}</p>
+      <img className={style.image} src={image} alt={title}/>
     </div>
   );
 }
